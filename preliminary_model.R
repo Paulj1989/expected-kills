dd323905c30a1bc0b4d7c7ce1122759a17783bba
Paul Johnson <paulrjohnson1989@gmail.com>
2020-12-15 12:02:49 -0600

    Fix for consistency with tidyverse style

--- preliminary_model.R
+++ preliminary_model.R
@@ -1,4 +1,4 @@
-pacman::p_load(datavolley, tidyverse, tidymodels)
+pacman::p_load(tidyverse, tidymodels)
 
 # load data
 power5 <- read_csv("power5_2018.csv")
@@ -6,34 +6,34 @@ power5 <- read_csv("power5_2018.csv")
 # rename, filter, and select relevant columns
 df <- power5 %>%
   # rename all columns to lowercase
-  rename_all(tolower)%>%
-  rename("prev_contact" = previouscontact,
-         "prevprev_contact" = previouspreviouscontact) %>%
+  rename_all(tolower) %>%
+  rename(
+    "prev_contact" = previouscontact,
+    "prevprev_contact" = previouspreviouscontact
+  ) %>%
   # filter rows to return model subset
-  filter(gender=="women" & skill=="A") %>%
+  filter(gender == "women" & skill == "A") %>%
   filter(skillquality == "#" | skillquality == "=") %>%
   # remove grade from contact columns
-  mutate(prev_contact = str_sub(prev_contact, 1, 1),
-         prevprev_contact = str_sub(prevprev_contact, 1, 1)) %>%
+  mutate(
+    prev_contact = str_sub(prev_contact, 1, 1),
+    prevprev_contact = str_sub(prevprev_contact, 1, 1)
+  ) %>%
   # select columns that are relevant to the model
-  select("skillquality", "skilltype", "prev_contact",
-         "prevprev_contact","blockers", "extra",
-         "set_angle", "firstx", "firsty", "thirdx", "thirdy")
-
-# remove power5 dataframe to reduce memory consumption
-rm(power5)
-
-# check for imbalances
-df %>%
-  group_by(skillquality) %>%
-  count()
+  select(
+    "skillquality", "skilltype", "prev_contact",
+    "prevprev_contact", "blockers", "extra",
+    "set_angle", "firstx", "firsty", "thirdx", "thirdy"
+  )
 
 # split train/test data
 set.seed(456)
 
 train_test_split <-
-  initial_split(df, prob = 0.8,
-                strata = skillquality)
+  initial_split(df,
+    prob = 0.8,
+    strata = skillquality
+  )
 
 power5_train <- training(train_test_split)
 power5_test <- testing(train_test_split)
@@ -43,7 +43,7 @@ power5_recipe <-
   # identify the outcome and predictor variables
   recipe(skillquality ~ ., data = power5_train) %>%
   # dummy nominal variables (except for outcome)
-  step_dummy(all_nominal(), - skillquality) %>%
+  step_dummy(all_nominal(), -skillquality) %>%
   # apply BoxCox Transformation to all predictor variables
   step_BoxCox(all_predictors()) %>%
   # normalize predictor variables
@@ -72,4 +72,5 @@ pred_accuracy <-
   power5_test$skillquality %>%
   bind_cols(
     predict(xg_fit, new_data = power5_test),
-    predict(xg_fit, new_data = power5_test, type = "prob"))
+    predict(xg_fit, new_data = power5_test, type = "prob")
+  )
